0115c6d962144ccba620e50be08c917170301056
Bismarckz <mac@192.168.1.5>
2022-10-31 18:10:52 +0700

    2

---
+++ AlgoritmaPemograman-1/prelab2.4.py
@@ -0,0 +1,17 @@
+# panjang = int(input('Masukkan panjang (cm): '))
+# lebar = int(input('Masukkan lebar (cm): '))
+
+# print('Persegi panjang dengan panjang = {0} cm dan lebar = {1} cm mempunyai luas = {2} cm2 dan keliling = {3} cm.'.format(panjang, lebar, panjang*lebar, (panjang+lebar)*2))
+
+
+# import math
+
+# detikTotal = int(input('Masukkan waktu dalam detik: '))
+# jam = math.floor(detikTotal / 60 / 60)
+# menit = math.floor(detikTotal / 60 % 60)
+# detik = math.floor(detikTotal % 60 % 60 )
+
+# print('Berikut waktu dalam jam, menit, dan detik:\nJam: {0} \nMenit: {1} \nDetik: {2}'.format(jam,menit,detik))
+
+# totalPenjualan = int(input('Masukkan total penjualan tahun ini: '))
+# print('Keuntungan tahun ini adalah Rp. {0:,.2f}'.format(totalPenjualan*0.23))
