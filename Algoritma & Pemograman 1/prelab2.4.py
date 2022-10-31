9b80196a0c3b01b870e37b806b6c9bb3a30e922a
Bismarckz <mac@192.168.1.5>
2022-10-31 16:50:01 +0700

    first

---
+++ Algoritma & Pemograman 1/prelab2.4.py
@@ -0,0 +1,4 @@
+ujian1 = float(input('Masukkan panjang (cm): '))
+ujian2 = float(input('Masukkan lebar (cm): '))
+
+print('Persegi panjang dengan panjang = 15 cm dan lebar = 5 cm mempunyai luas = 75 cm2 dan keliling = 40 cm.'.format(rata2))
