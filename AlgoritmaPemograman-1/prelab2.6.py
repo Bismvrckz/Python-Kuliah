a0b40307766678aaa3bb8d3f3cacec1da4ec7703
Bismarckz <mac@192.168.1.5>
2022-11-01 22:20:30 +0700

    2.6

---
+++ AlgoritmaPemograman-1/prelab2.6.py
@@ -0,0 +1,34 @@
+import math
+
+
+# Fungsi hitung_populasi
+# [2] Tuliskan parameter-parameter yang diperlukan
+def hitung_populasi(pop):
+    # [3] Hitung populasi setelah waktu t. Gunakan fungsi dalam module math.
+    # Bulatkan ke bawah hasil penghitungan (hilangkan nilai desimalnya). Misal: hasil penghitungan 754595.685 maka nilai
+    # yang dikembalikan fungsi ini adalah 754595.
+    
+    
+    
+    
+    
+
+# Fungsi main
+def main():
+    # [4] Minta input pengguna untuk populasi awal. Prompt 'Masukkan populasi awal: '
+    populasi = input('Masukkan populasi awal: ')
+    
+    # [5] Minta input pengguna untuk waktu. Prompt 'Masukkan waktu dalam tahun: '
+    waktu = input('Masukkan waktu dalam tahun: ')
+    
+    # [6] Hitung populasi setelah waktu t dengan memanggil fungsi hitung_populasi
+    hitung_populasi(populasi,waktu)
+
+
+
+    # [7] Tampilkan populasi akhir: Populasi akhir = <nilai_populasi_akhir>
+    
+    
+    
+# Panggil fungsi main
+main()
