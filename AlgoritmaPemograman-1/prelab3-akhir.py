389c1d78798f7456fc91be51f9c5b2f1f565cc24
Bismarckz <mac@192.168.1.5>
2022-11-02 09:43:04 +0700

    y

---
+++ AlgoritmaPemograman-1/prelab3-akhir.py
@@ -0,0 +1,30 @@
+# import math
+
+# def main():
+#     detikTotal = int(input('Masukkan jumlah detik: '))
+#     hari = math.floor(detikTotal / 60 / 60 / 24)
+#     jam = math.floor(detikTotal / 60 / 60 % 24)
+#     menit = math.floor(detikTotal / 60 % 60 )
+#     detik = math.floor(detikTotal % 60 % 60 )
+
+#     if hari:
+#         print('{0} hari {1} jam {2} menit {3} detik'.format(hari,jam,menit,detik))
+#     elif jam:
+#         print('{0} jam {1} menit {2} detik'.format(jam,menit,detik))
+#     elif menit:
+#         print('{0} menit {1} detik'.format(menit,detik))
+#     else:
+#         print('{0} detik'.format(detik))
+# main()
+
+# def main():
+#     lanjut = 'y'
+
+#     while lanjut == 'y':
+#         angka1 = int(input('Masukkan angka 1: '))
+#         angka2 = int(input('Masukkan angka 2: '))
+#         jumlah = angka1 + angka2
+#         print(f'Jumlah = {jumlah}')
+#         lanjut = input('Masukkan y untuk melanjutkan: ')
+    
+# main()
---
+++ AlgoritmaPemograman-1/prelab3.4.py
@@ -0,0 +1,31 @@
+# def main():
+#     pembelian = int(input('Masukkan banyak pembelian: '))
+    
+#     if pembelian > 99:
+#         diskon = 0.5
+#     elif pembelian > 49:
+#         diskon = 0.4
+#     elif pembelian > 19:
+#         diskon = 0.3
+#     elif pembelian > 10:
+#         diskon = 0.2
+#     else:
+#         diskon = 0
+    
+#     if diskon != 0:
+#         print('Diskon = Rp.{0:,.2f}'.format(pembelian*diskon*990000))
+#         print('Total = Rp.{0:,.2f}'.format(pembelian*990000-pembelian*diskon*990000))
+#     else:
+#         print('Total = Rp.{0:,.2f}'.format(pembelian*990000))
+
+# main()
+
+# def main():
+#     gaji = int(input('Masukkan gaji Anda: '))
+#     lamaBekerja = int(input('Lama bekerja (tahun): '))
+#     if lamaBekerja >= 2 and gaji >= 3000000:
+#         print('Anda terkualifikasi untuk menerima pinjaman!')
+#     else:
+#         print('Anda tidak terkualifikasi untuk menerima pinjaman!')
+    
+# main()
