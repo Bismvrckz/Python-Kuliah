0ea61e431014f2ec361a104b269dec626a23ff42
Bismarckz <mac@192.168.1.5>
2022-11-02 11:30:12 +0700

    kartesian

---
+++ MatematikaInformatika-1/index.py
@@ -0,0 +1,28 @@
+# A = [1,6,8,9,11]                                      
+# for x in range(5):                                     
+#   print(A[x])
+
+# A = {2, -3, 5, -1}                          
+# B = {0, 2, -1}                              
+# # Gabungan A dan B
+# A_u_B = A.union(B)                                        
+# # Irisan A dan B
+# A_n_B = A.intersection(B)                                 
+# # Selisih A - B
+# A_min_B = A.difference(B)                                   
+# # Selisih B - A
+# B_min_A = B.difference(A)                                   
+# print('A =', A)
+# print('B =', B)
+# print('Gabungan A dan B =', A_u_B)
+# print('Irisan A dan B =', A_n_B)
+# print('A - B =', A_min_B) 
+# print('B - A =', B_min_A)
+
+# def kali_kartesian(x,y):
+#     product = set()
+#     for a in x:
+#         for b in y:
+#             product.add((a, b))
+#     return product
+    
