c7b55cd97cf31faef3711fdc9557d06345c7b3a3
Bismarckz <mac@192.168.1.5>
2022-11-12 09:49:43 +0700

    slamet kopling

--- AlgoritmaPemograman-1/prelab4.5.py
+++ AlgoritmaPemograman-1/prelab4.5.py
@@ -5,3 +5,14 @@
 #         string_akhir += input_string[-(i+1)]
     
 #     print(string_akhir)
+
+
+# def banyak_huruf_besar(input_string):
+
+#     jumlah_huruf = 0
+
+#     for i in range(len(input_string)):
+#         if input_string[i].isupper():
+#             jumlah_huruf += 1
+
+#     return jumlah_huruf
