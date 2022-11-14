6b89782ff53dd6ae4e3ad11f1ec034668e733668
Bismvrckz <ahmadfayruzsyamil33@gmail.com>
2022-11-14 22:41:33 +0700

    gundar kacau

---
+++ AlgoritmaPemograman-1/prelab5.1.py
@@ -0,0 +1,28 @@
+# def main():
+#     output_file = open('kota.txt','w')
+#     output_file.write('Bandung\n')
+#     output_file.write('Jakarta\n')
+#     output_file.write('Depok\n')
+#     output_file.close()
+# main()
+
+# def main():
+#     kota_file = open('kota.txt','a')
+#     kota_file.write('Surabaya\n')
+#     kota_file.write('Medan\n')
+#     kota_file.close()
+# main()
+
+# def main():
+#     kota_baru = input("Masukkan nama kota: ")
+#     kota_file = open('kota.txt','a')
+#     kota_file.write(kota_baru)
+#     kota_file.close()
+#     print('Data telah ditambahkan ke file kota.txt.')
+# main()
+
+# def main():
+#     with open('kota.txt','r') as kota_file:
+#         isi_file = kota_file.read()
+#         print(isi_file)
+# main()
